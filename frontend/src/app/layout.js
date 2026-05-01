28658e2f9a083384445a5568a5bb10544d7c8974
cderry8 <derrickcyubahiroo@gmail.com>
2026-05-01 12:43:13 +0200

    added vercel analytics

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import Navbar from "./components/static/Navbar";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: "UTF League - EAFC Pro Clubs League Management",
